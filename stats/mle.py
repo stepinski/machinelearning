f5c2625e9373999c9eb05fbf56ef6e1dc746a59d
stepinski <stepinski@gmnail.com>
2021-02-23 14:56:42 +0100

    mle function with pvalue

--- stats/mle.py
+++ stats/mle.py
@@ -1,7 +1,16 @@
 import math
 import scipy.stats as st
+from scipy.stats.distributions import chi2
+
+def likelihood_ratio(t,c,tcount,all):
+    return -2*math.log(st.binom.pmf(t,tcount,(t+c)/all) * st.binom.pmf(c,all-tcount,(t+c)/all) / (st.binom.pmf(t,tcount,t/tcount) * st.binom.pmf(c,all-tcount,c/(all-tcount))))
+
 pi_mle=102/62000
 pi_mle_treat=39/31000
 pi_mle_control=63/31000
-lmbd=-2*math.log(st.binom.pmf(39,31000,102/62000) * st.binom.pmf(63,31000,102/62000) / (st.binom.pmf(39,31000,39/31000) * st.binom.pmf(63,31000,63/31000)))
-print(lmbd)
+#likelihood ratio
+LR=-2*math.log(st.binom.pmf(39,31000,102/62000) * st.binom.pmf(63,31000,102/62000) / (st.binom.pmf(39,31000,39/31000) * st.binom.pmf(63,31000,63/31000)))
+print(likelihood_ratio(39,63,31000,62000))
+print(LR)
+pvalue = chi2.sf(LR, 1)
+print('pvalue %.6f'%pvalue)
