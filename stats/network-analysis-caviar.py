9f13a1216232d0825051bcd736e62749a0377f35
stepinski <stepinski@gmail.com>
2021-04-16 15:02:31 +0200

    connected components analysis

--- stats/network-analysis-caviar.py
+++ stats/network-analysis-caviar.py
@@ -112,6 +112,109 @@ rels=crimes.set_index('CrimeIdentifier').join(crimestmp.set_index('CrimeIdentifi
 
 
 [print(ofl) for (ofl, ofr) in zip(df['OffenderIdentifier_l'], df['OffenderIdentifier_r'])]
+row = rels['OffenderIdentifier_l'].to_numpy()
+col = rels['OffenderIdentifier_r'].to_numpy()
+  
+lng = len(row)  
+# taking data
+data = np.ones(lng)
+  
+# creating sparse matrix
+cooffend_matrix = csr_matrix((data, (row, col)),shape = (row.max() + 1, col.max() + 1))
+cooffend_matrix[cooffend_matrix > 0] = 1
+cooffend_matrix.setdiag(0)
+cooffend_matrix.eliminate_zeros() # To avoid self loops since setdiag(0) does not itself change the sparsity pattern
+coofs =nx.from_scipy_sparse_matrix(cooffend_matrix)
 
 
-crimes=dfr[['OffenderIdentifier', 'CrimeIdentifier']].to_numpy()
+
+coofs.number_of_nodes() 
+coofs.number_of_edges()  
+len(list(nx.isolates(coofs)))
+g=coofs.copy()
+g.remove_nodes_from(list(nx.isolates(g)))
+sum([x>=100 for x in sorted([d for n, d in g.degree()], reverse=True)])  
+
+len(sorted(nx.connected_components(g), key=len, reverse=True) )
+
+
+
+cntpairs=rels.groupby(['OffenderIdentifier_r','OffenderIdentifier_l']).size().reset_index(name='counts').sort_values('counts') 
+nonones=cntpairs.loc[cntpairs['counts']>1]
+
+row2 = nonones['OffenderIdentifier_l'].to_numpy()
+col2 = nonones['OffenderIdentifier_r'].to_numpy()
+  
+lng2 = len(row2)  
+# taking data
+data2 = np.ones(lng2)
+  
+# creating sparse matrix
+cooffend_matrix2 = csr_matrix((data2, (row2, col2)),shape = (row2.max() + 1, col2.max() + 1))
+cooffend_matrix2[cooffend_matrix2 > 0] = 1
+cooffend_matrix2.setdiag(0)
+cooffend_matrix2.eliminate_zeros() # To avoid self loops since setdiag(0) does not itself change the sparsity pattern
+coofs2 =nx.from_scipy_sparse_matrix(cooffend_matrix2)
+coofs2.remove_nodes_from(list(nx.isolates(coofs2)))
+
+
+
+
+cntpairs2=rels.groupby(['OffenderIdentifier_r','OffenderIdentifier_l']).size().reset_index(name='counts').sort_values('counts') 
+nonones2=cntpairs.loc[cntpairs['counts']==1]
+
+row3 = nonones2['OffenderIdentifier_l'].to_numpy()
+col3 = nonones2['OffenderIdentifier_r'].to_numpy()
+  
+lng3 = len(row3)  
+# taking data
+data3 = np.ones(lng3)
+  
+# creating sparse matrix
+cooffend_matrix3 = csr_matrix((data3, (row3, col3)),shape = (row3.max() + 1, col3.max() + 1))
+cooffend_matrix3[cooffend_matrix3 > 0] = 1
+cooffend_matrix3.setdiag(0)
+cooffend_matrix3.eliminate_zeros() # To avoid self loops since setdiag(0) does not itself change the sparsity pattern
+coofs3 =nx.from_scipy_sparse_matrix(cooffend_matrix3)
+coofs3.remove_nodes_from(list(nx.isolates(coofs3)))
+
+#coofs2 is gr
+#coofs3 is gnr
+
+
+sum([x>=100 for x in sorted([d for n, d in g.degree()], reverse=True)])  
+
+len(sorted(nx.connected_components(coofs2), key=len, reverse=True) )
+len(sorted(nx.connected_components(coofs3), key=len, reverse=True) )
+
+lst1=[len(c) for c in sorted(nx.connected_components(coofs2), key=len, reverse=True)] 
+sum(lst1)/len(lst1) 
+
+lst2=[len(c) for c in sorted(nx.connected_components(coofs3), key=len, reverse=True)] 
+sum(lst2)/len(lst2) 
+
+max1=max(nx.connected_components(coofs), key=len)
+max2=max(nx.connected_components(coofs2), key=len)
+max3=max(nx.connected_components(coofs3), key=len)
+
+len(max2)/sum(lst1)
+len(max3)/sum(lst2) 
+
+
+print("{:.5f}".format(nx.density(max1)))
+
+print("{:.5f}".format(nx.density(max2)))
+
+print("{:.5f}".format(nx.density(max3)))
+
+
+S = [(c,coofs.subgraph(c).copy())for c in nx.connected_components(coofs)]
+nx.density(max(S,key=lambda item:len(item[0]))[1])  
+
+
+S2 = [(c,coofs2.subgraph(c).copy())for c in nx.connected_components(coofs2)]
+nx.density(max(S2,key=lambda item:len(item[0]))[1])  
+
+S3 = [(c,coofs3.subgraph(c).copy())for c in nx.connected_components(coofs3)]
+nx.density(max(S3,key=lambda item:len(item[0]))[1])  
+
