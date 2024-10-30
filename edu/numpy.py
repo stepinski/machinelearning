ebbf04c5cf1b74f08b8fb2a4592f633a3016326a
stepinski <stepinski@gmail.com>
2024-10-30 15:16:50 +0100

    recap

---
+++ edu/numpy.py
@@ -0,0 +1,6 @@
+arr=np.array([np.nan,2,3,4,5],dtype=np.float32)# CODE HERE
+arr2=arr.copy()
+arr2[0]=10
+float_arr=np.array([1,5.4,3])
+float_arr2=arr2.astype(np.float32)
+matrix=np.array([[1,2,3],[4,5,6]],dtype=np.float32)
